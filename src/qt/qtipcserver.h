85492ce88eb6cfe234697d59e3e5c36992a6cb80
devilcoin <devilcoin@protonmail.com>
2016-04-01 19:16:59 +0200

    dxc

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define DevilCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "DevilCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
